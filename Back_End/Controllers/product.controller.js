ea823cfbb8bf85577576f0968289858451d4ba0b
GarielleC <141339259+GarielleC@users.noreply.github.com>
2023-11-27 09:32:33 +0100

    back_end modificationCorrectionErrorProductController

--- Back_End/Controllers/product.controller.js
+++ Back_End/Controllers/product.controller.js
@@ -11,7 +11,7 @@ const productController = {
                     res.status(200).json(product);
                 } catch (error) {
                     console.error(error);
-                    res.status(500).send(error.message);
+                    res.status(500).send({ error: error.message });
                 }
             },
     // getProduct: async (req, res) => {
